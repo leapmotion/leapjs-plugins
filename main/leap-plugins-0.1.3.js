50b7835d858f9ed6806c53bfaad2d7e379bee3ff
Peter Ehrlich <peter.i.ehrlich@gmail.com>
2014-03-27 23:42:32 -0700

    [wip] Add version check plugin
    Currently has issues with auto-reconnection on disconnect.

--- main/leap-plugins-0.1.3.js
+++ main/leap-plugins-0.1.3.js
@@ -1,5 +1,5 @@
 /*    
- * LeapJS-Plugins  - v0.1.3 - 2014-03-12    
+ * LeapJS-Plugins  - v0.1.3 - 2014-03-27    
  * http://github.com/leapmotion/leapjs-plugins/    
  *    
  * Copyright 2014 LeapMotion, Inc    
@@ -241,6 +241,47 @@ More info on vec3 can be found, here: http://glmatrix.net/docs/2.2.0/symbols/vec
     Leap.Controller.plugin('screenPosition', screenPosition);
   } else {
     module.exports.screenPosition = screenPosition;
+  }
+
+}).call(this);
+
+//CoffeeScript generated from main/version-check/leap.version-check.coffee
+(function() {
+  var versionCheck;
+
+  versionCheck = function(scope) {
+    scope.alert || (scope.alert = false);
+    scope.requiredProtocolVerion || (scope.requiredProtocolVerion = 6);
+    scope.disconnect || (scope.disconnect = true);
+    this.on('ready', function() {
+      var current, required;
+      required = scope.requiredProtocolVerion;
+      current = this.connection.opts.requestProtocolVersion;
+      if (current < required) {
+        console.warn("Protocol Version too old. v" + required + " required, v" + current + " available.");
+        this.emit('versionCheck.outdated', {
+          required: required,
+          current: current,
+          disconnect: scope.disconnect
+        });
+        if (scope.disconnect) {
+          clearInterval(this.connection.reconnectionTimer);
+          this.disconnect();
+        }
+        if (scope.alert) {
+          return alert("Your Leap Software version is out of date.  Visit http://www.leapmotion.com/setup to update");
+        }
+      }
+    });
+    return {};
+  };
+
+  if ((typeof Leap !== 'undefined') && Leap.Controller) {
+    Leap.Controller.plugin('versionCheck', versionCheck);
+  } else if (typeof module !== 'undefined') {
+    module.exports.versionCheck = versionCheck;
+  } else {
+    throw 'leap.js not included';
   }
 
 }).call(this);
---
+++ main/version-check/leap.version-check.coffee
@@ -0,0 +1,37 @@
+versionCheck = (scope)->
+  scope.alert ||= false
+  scope.requiredProtocolVerion ||= 6
+  scope.disconnect ||= true
+
+  @on 'ready', ->
+    required = scope.requiredProtocolVerion
+    current = @connection.opts.requestProtocolVersion
+
+    if current < required
+      console.warn "Protocol Version too old. v#{required} required, v#{current} available."
+
+      @emit('versionCheck.outdated', {
+        required: required
+        current: current
+        disconnect: scope.disconnect
+      })
+
+      if scope.disconnect
+        # due to LeapJS implementation details, a call to disconnect within 1000ms of connection will result in
+        # automatic reconnection.  We disable this behavior below.
+        # https://github.com/leapmotion/leapjs/blob/master/lib/connection/base.js#L64
+        clearInterval(@connection.reconnectionTimer)
+        @disconnect()
+
+      if scope.alert
+        alert("Your Leap Software version is out of date.  Visit http://www.leapmotion.com/setup to update")
+
+  {}
+  
+  
+if (typeof Leap != 'undefined') && Leap.Controller
+  Leap.Controller.plugin 'versionCheck', versionCheck
+else if (typeof module != 'undefined')
+  module.exports.versionCheck = versionCheck
+else
+  throw 'leap.js not included'  
